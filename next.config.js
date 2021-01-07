1b57b2ae48d0ae0e24c074b7e3f0b2d615cd03c7
Ron Houben <ron.houben@accenture.com>
2021-01-07 17:47:53 +0100

    feat: added extra pwa options to next.config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ const runtimeCaching = require('next-pwa/cache')
 module.exports = withPWA({
   pwa: {
     // disable PWA when running in local dev mode
-    disable: process.env.NODE_ENV === 'production' ? false : true,
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    scope: '/',
     dest: 'public',
     runtimeCaching,
   },
