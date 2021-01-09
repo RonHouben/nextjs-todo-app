0ef37faafe42e9ced5f4dff94607881689066495
Ron Houben <ron.houben@accenture.com>
2021-01-09 21:47:41 +0100

    feat: corrected env.d.ts

--- utils/interfaces/env.d.ts
+++ utils/interfaces/env.d.ts
@@ -4,16 +4,17 @@ declare global {
       NODE_ENV: 'development' | 'production'
 
       // Firebase variables
-      FIREBASE_API_KEY: string
-      FIREBASE_AUTH_DOMAIN: string
-      FIREBASE_PROJECT_ID: string
-      FIREBASE_DB_URL: string
-      FIREBASE_STORAGE_BUCKET: string
-      FIREBASE_MESSAGING_SENDER_ID: string
-      FIREBASE_APP_ID: string
-      FIREBASE_MEASUREMENT_ID: string
+      NEXT_PUBLIC_FIREBASE_API_KEY: string
+      NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: string
+      NEXT_PUBLIC_FIREBASE_PROJECT_ID: string
+      NEXT_PUBLIC_FIREBASE_DB_URL: string
+      NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: string
+      NEXT_PUBLICFIREBASE_MESSAGING_SENDER_ID: string
+      NEXT_PUBLIC_FIREBASE_APP_ID: string
+      NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: string
       FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY: string
       FIREBASE_CLIENT_EMAIL: string
+      NEXT_PUBLIC_FIRESTORE_EMULATOR_HOST?: string
     }
   }
 }
