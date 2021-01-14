5dfd7cb2fc6c19b14615d29a7a345ea5233288f6
Ron Houben <ron.houben85@gmail.com>
2021-01-14 22:23:21 +0100

    feat: removed reactfire from _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,20 @@
-import { AppProps } from 'next/app'
-import '../styles/global.css'
-import { ThemeProvider } from 'next-themes'
-import { FirebaseAppProvider } from 'reactfire'
-import { firebaseApp } from '../lib/firebaseClient'
-import { Provider as AuthProvider } from 'next-auth/client'
+import { AppProps } from "next/app";
+import "../styles/global.css";
+import { ThemeProvider } from "next-themes";
+import { Provider as AuthProvider } from "next-auth/client";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider
-      attribute='class'
-      themes={['light', 'dark']}
-      defaultTheme='light'
+      attribute="class"
+      themes={["light", "dark"]}
+      defaultTheme="light"
     >
       <AuthProvider session={pageProps.sessions}>
-        <FirebaseAppProvider firebaseApp={firebaseApp}>
-          <Component {...pageProps} />
-        </FirebaseAppProvider>
+        <Component {...pageProps} />
       </AuthProvider>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
