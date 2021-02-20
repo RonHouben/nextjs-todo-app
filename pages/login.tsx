3247e99fd64e11801b5e0bc76c0db9086145d221
Ron Houben <ron.houben85@gmail.com>
2021-02-20 12:27:41 +0100

    wip: added functionality for email and password registration but disabled it for now since I'm still testing it. Also added an error toast when clicking an invalid signin provider

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import firebase from 'firebase/app'
 import { withAuthUser, AuthAction } from 'next-firebase-auth'
 import Layout from '../components/Layout'
@@ -8,22 +8,54 @@ import GithubIconButton from '../components/IconButton'
 import GoogleIconButton from '../components/IconButton'
 import { useTheme } from 'next-themes'
 import LoadingScreen from '../components/LoadingScreen'
+import { toast } from 'react-toastify'
 
 function LoginPage() {
   const { theme } = useTheme()
 
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  // set firebase analytics current screen
   useEffect(() => {
     firebase.analytics().setCurrentScreen('login_screen')
   }, [])
 
+  // handle login redirect result
+  useEffect(() => {
+    firebase
+      .auth()
+      .getRedirectResult()
+      .then((result) => console.log(result))
+      .catch((error) => toast.error(error.message))
+  }, [])
+
   // handlers
-  const handleLogin = (provider: 'github' | 'google') => {
+  interface LoginProps {
+    provider: 'email-password' | 'github' | 'google'
+    email?: string
+    password?: string
+  }
+
+  const handleLogin = async ({ provider, email, password }: LoginProps) => {
     // log analytics event
-    firebase.analytics().logEvent('login', { provider })
+    firebase.analytics().logEvent('login', { provider, email })
     // call signIn function
+    if (provider === 'email-password' && email && password) {
+      try {
+        await firebase.auth().signInWithEmailAndPassword(email, password)
+      } catch (error) {
+        if (error.code === 'auth/wrong-password') {
+          toast.error('Wrong password.')
+        } else {
+          toast.error(error.message)
+        }
+      }
+
+      return
+    }
     if (provider === 'github') {
       firebase.auth().signInWithRedirect(new firebase.auth.GithubAuthProvider())
-
       return
     }
     if (provider === 'google') {
@@ -43,20 +75,30 @@ function LoginPage() {
       >
         <h2 className="">Log In</h2>
         <Textbox
-          // disabled
-          value=""
+          disabled
+          value={email}
+          onChange={setEmail}
           placeholder="Email address"
           type="email"
           border
         />
         <Textbox
-          // disabled
-          value=""
+          disabled
+          value={password}
+          onChange={setPassword}
           placeholder="Password"
           type="password"
           border
         />
-        <button className="w-full p-2 border-2 rounded-lg">Log in</button>
+        <button
+          disabled
+          className="w-full p-2 border-2 rounded-lg"
+          onClick={() =>
+            handleLogin({ provider: 'email-password', email, password })
+          }
+        >
+          Log in
+        </button>
         <div className="flex">
           <GithubIconButton
             alt="Sign In with GitHub"
@@ -66,13 +108,13 @@ function LoginPage() {
                 : '/icons/github-icon-light.png'
             }
             size="lg"
-            onClick={() => handleLogin('github')}
+            onClick={() => handleLogin({ provider: 'github' })}
           />
           <GoogleIconButton
             alt="Sign in with Google"
             src="/icons/google-icon.png"
             size="lg"
-            onClick={() => handleLogin('google')}
+            onClick={() => handleLogin({ provider: 'google' })}
           />
         </div>
       </Paper>
