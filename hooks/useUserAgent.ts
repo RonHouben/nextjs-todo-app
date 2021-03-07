fbf2eddf6884d8c0f33e31f5ddd00bb01b5eee06
Ron Houben <ron.houben85@gmail.com>
2021-03-07 11:09:42 +0100

    fix: replaced the userAgentContext with just the hook since we can get the userAgent via the client using `navigator.userAgent`

--- hooks/useUserAgent.ts
+++ hooks/useUserAgent.ts
@@ -1,8 +1,34 @@
-import { useContext } from 'react'
-import {
-  IUserAgentContext,
-  UserAgentContext,
-} from '../providers/UserAgentProvider'
+import { useEffect, useState } from 'react'
 
-export const useUserAgent = () =>
-  useContext<IUserAgentContext>(UserAgentContext)
+interface IUserAgentResult {
+  browser: 'Chrome' | 'Safari' | 'Firefox' | undefined
+  isChrome: boolean
+  isSafari: boolean
+  isFirefox: boolean
+}
+
+export const useUserAgent = (): IUserAgentResult => {
+  const [browser, setBrowser] = useState<IUserAgentResult['browser']>()
+
+  if (process.browser && navigator) {
+    useEffect(() => {
+      setBrowser(getBrowser(navigator.userAgent))
+    }, [navigator.userAgent])
+  }
+
+  return {
+    browser,
+    isChrome: browser === 'Chrome',
+    isFirefox: browser === 'Firefox',
+    isSafari: browser === 'Safari',
+  }
+}
+
+// helper functions
+const getBrowser = (userAgent: NavigatorID['userAgent']) => {
+  const regexMatch = userAgent
+    ? userAgent.match(/Chrome|Safari|Firefox/)
+    : undefined
+
+  return regexMatch ? (regexMatch[0] as IUserAgentResult['browser']) : undefined
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,19 @@
-import { IncomingHttpHeaders } from 'http'
-import { AppContext, AppProps } from 'next/app'
+import { AppProps } from 'next/app'
 import React from 'react'
 import initAuth from '../lib/initAuth'
 import ChakraProvider from '../providers/Chakra'
-import { UserAgentProvider } from '../providers/UserAgentProvider'
 
 initAuth()
 interface Props extends AppProps {
   cookies: string
-  userAgent: IncomingHttpHeaders['user-agent']
 }
 
-function App({ Component, pageProps, cookies, userAgent }: Props) {
+function App({ Component, pageProps, cookies }: Props) {
   return (
-    <UserAgentProvider userAgent={userAgent}>
-      <ChakraProvider cookies={cookies}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </UserAgentProvider>
+    <ChakraProvider cookies={cookies}>
+      <Component {...pageProps} />
+    </ChakraProvider>
   )
-}
-
-// SSR to get the client browser. This can later be used to apply styling for a specific browser
-App.getInitialProps = async (app: AppContext) => {
-  return { userAgent: app.ctx.req?.headers['user-agent'] }
 }
 
 export default App
